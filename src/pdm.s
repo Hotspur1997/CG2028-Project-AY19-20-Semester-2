e1e20982ae920d97cec26c06bf401a292a70ede1
Weng Kexin <42372486+Weng-Kexin@users.noreply.github.com>
2020-04-16 12:19:44 +0800

    removed unneeded part (constants)

--- src/pdm.s
+++ src/pdm.s
@@ -4,28 +4,30 @@
  	.align 2
  	.global	pdm
  	.thumb_func
-@  CG2028 Assignment skeleton
+@  CG2028 Assignment
+@  Lab Group B02-12
 @  ECE, NUS
+
 pdm:
 @  Write Probability of Detection function in assembly language here
-@  Currently, nothing is done and this function returns straightaway
-	PUSH {R4-R8}
+
+	PUSH {R4-R8}@push registers to backup register content
 	CMP R0, #0  @check if the value stored inside R8 is 0
 	BEQ Finish  @branch to Finish block if R0 is equal to 0
 	MOV R5, #0 	@initialise register R5 to be 0 ie Denominator Accumulator
 	MOV R6, #0 	@initialise register R6 to be 0
 	MOV R7, #0  @initialise the register 7 to 0
 	MOV R8, #0 	@initialise the register 8 to 0
-	
+
 Add:
 	LDR R6, [R2], #4  @load the value of contents of register R2 into R6 and increment R2 by 4
 	ADD R5, R6 	@add the contents of R6 to R5
-	CMP R7, R8 @check if R7 and R8 are equal 
+	CMP R7, R8  @check if R7 and R8 are equal
 	IT EQ
 	MOVEQ R4, R6  @if R7 and R8 are equal, move the value of R6 into R4
-	ADD R7, #1 	@increment R7 by 1
-	CMP R7, R0 	@check if R7 is equal to R0
-	BEQ Evaluate @branch to eval
+	ADD R7, #1    @increment R7 by 1
+	CMP R7, R0 	  @check if R7 is equal to R0
+	BEQ Evaluate  @branch to eval
 	B Add
 
 Evaluate:
@@ -40,9 +42,5 @@ Evaluate:
 	B Add 	@branch back to add
 
 Finish:
-	POP {R4-R8}
+	POP {R4-R8} @pop registers to restore register content
   	BX LR
-
-@ Define constant values
-CONST: .word 123
-
