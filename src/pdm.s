5047f9714202cbb4741bec46eb4fc06ab8c62663
Weng Kexin <42372486+Weng-Kexin@users.noreply.github.com>
2020-04-16 15:06:00 +0800

    Update pdm.s

--- src/pdm.s
+++ src/pdm.s
@@ -6,23 +6,25 @@
  	.thumb_func
 @  CG2028 Assignment
 @  Lab Group B02-12
+@  Marc Phua Hsiao Meng A0183219A
+@  Weng Kexin A0187566L
 @  ECE, NUS
 
 pdm:
 @  Write Probability of Detection function in assembly language here
 
 	PUSH {R4-R8}@push registers to backup register content
-	CMP R0, #0  @check if the value stored inside R8 is 0
+	CMP R0, #0  @check if the value stored inside R0 is 0
 	BEQ Finish  @branch to Finish block if R0 is equal to 0
 	MOV R5, #0 	@initialise register R5 to be 0 ie Denominator Accumulator
 	MOV R6, #0 	@initialise register R6 to be 0
-	MOV R7, #0  @initialise the register 7 to 0
-	MOV R8, #0 	@initialise the register 8 to 0
+	MOV R7, #0  @initialise register R7 to be 0
+	MOV R8, #0 	@initialise register R8 to be 0
 
 Add:
 	LDR R6, [R2], #4  @load the value of contents of register R2 into R6 and increment R2 by 4
-	ADD R5, R6 	@add the contents of R6 to R5
-	CMP R7, R8  @check if R7 and R8 are equal
+	ADD R5, R6        @add the contents of R6 to R5
+	CMP R7, R8        @check if R7 and R8 are equal
 	IT EQ
 	MOVEQ R4, R6  @if R7 and R8 are equal, move the value of R6 into R4
 	ADD R7, #1    @increment R7 by 1
@@ -31,7 +33,7 @@ Add:
 	B Add
 
 Evaluate:
-	MUL R4, R3 	@multiply R4 by the scale value
+	MUL R4, R3 	        @multiply R4 by the scale value
 	UDIV R5, R4, R5 	@divide the contents in R4 by the contents in R5
 	STR R5, [R1], #4 	@store the result of R5 in the memory location of R1 and increment R1 by 4
 	MOV R5, #0 	@reset register R8 to be 0
@@ -39,7 +41,7 @@ Evaluate:
 	ADD R8, #1 	@increment R8 by 1 to move to the next row
 	CMP R8, R0 	@set the flag if R8 is equal to M
 	BEQ Finish 	@branch to fin if flag is true
-	B Add 	@branch back to add
+	B Add 	    @branch back to add
 
 Finish:
 	POP {R4-R8} @pop registers to restore register content
