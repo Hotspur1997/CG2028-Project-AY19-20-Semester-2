eddc89085b8ea28469c05445e06fe4798866490d
hotspur1997 <hotspur1997@gmail.com>
2020-04-22 19:57:58 +0800

    removed some redundant files

--- Assignment1_Friday_Grp 12_A0183219A_A0187566L_Workspace/CG2028AssignS2AY1920/Debug/src/cr_startup_lpc17.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc17.d: ../src/cr_startup_lpc17.c \
- C:\Users\ASUS\Desktop\CG2028workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
-
-C:\Users\ASUS\Desktop\CG2028workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
